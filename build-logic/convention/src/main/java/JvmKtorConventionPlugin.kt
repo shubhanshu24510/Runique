3bff64c562a8b16b093e70ea7d00f1875e3999a5
Shubhanshu Singh <shubhanshusingh2451@gmail.com>
2024-05-01 09:22:07 +0530

    gradle build completed.

---
+++ build-logic/convention/src/main/java/JvmKtorConventionPlugin.kt
@@ -0,0 +1,18 @@
+import com.shubhans.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class JvmKtorConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.apply {
+                apply("org.jetbrains.kotlin.plugin.serialization")
+                dependencies {
+                    "implementation"(libs.findBundle("ktor").get())
+                }
+            }
+        }
+
+    }
+}
